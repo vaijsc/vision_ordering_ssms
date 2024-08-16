03067dda2241557158df9e62ac1b1bac217b0c27
duc-anh-2002 <nguyenducanh909.bkhn@gmail.com>
2024-08-16 23:05:14 +0700

    update train original

--- mambavision/entries/main.sh
+++ mambavision/entries/main.sh
@@ -3,19 +3,7 @@
 #SBATCH --error=/lustre/scratch/client/vinai/users/phinh2/workspace/mambavision_1/mambavision/result/mambaV.txt
 #SBATCH --nodes=1
 #SBATCH --gpus-per-node=1
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-#SBATCH --nodelist=sdc2-hpc-dgx-a100-018
-=======
 #SBATCH --nodelist=sdc2-hpc-dgx-a100-019
->>>>>>> 39a87a24704883c4065a81b305c48220b9a260b0
-=======
-#SBATCH --nodelist=sdc2-hpc-dgx-a100-019
->>>>>>> 39a87a24704883c4065a81b305c48220b9a260b0
-=======
-#SBATCH --nodelist=sdc2-hpc-dgx-a100-019
->>>>>>> 39a87a24704883c4065a81b305c48220b9a260b0
 #SBATCH --mem-per-gpu=50G
 #SBATCH --cpus-per-gpu=40
 #SBATCH --partition=research
@@ -26,7 +14,7 @@
 #module load python/miniconda3/miniconda3
 eval "$(conda shell.bash hook)"
 #conda activate /lustre/scratch/client/vinai/users/anhnd81/envs/mambavision
-conda activate /lustre/scratch/client/vinai/users/phinh2/workspace/envs/simba
+conda activate /lustre/scratch/client/vinai/users/phinh2/workspace/envs/mambav
 #cd /home/anhnd81/anhnd81/workspace/mambavision_1/mambavision
 cd /lustre/scratch/client/vinai/users/phinh2/workspace/mambavision_1/mambavision
 echo "Current path is $PATH"
@@ -48,5 +36,5 @@ WR_LR=1e-6
 DR=0.38
 MESA=0.25
 RUN_FILE="/lustre/scratch/client/vinai/users/phinh2/workspace/mambavision_1/mambavision/train.py"
-torchrun --nproc_per_node=1 $RUN_FILE --mesa ${MESA} --input-size 3 224 224 --crop-pct=0.875 \
+torchrun --master_port 12351 --nproc_per_node=1 $RUN_FILE --mesa ${MESA} --input-size 3 224 224 --crop-pct=0.875 \
 --data_dir=$DATA_PATH --model $MODEL --amp --weight-decay ${WD} --drop-path ${DR} --batch-size $BS --tag $EXP --lr $LR --warmup-lr $WR_LR
