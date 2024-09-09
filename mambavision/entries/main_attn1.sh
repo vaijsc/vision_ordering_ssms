74daa7497dd454fba649cf7732f21862b79eaf2d
duc-anh-2002 <nguyenducanh909.bkhn@gmail.com>
2024-09-09 10:43:23 +0000

    update mambavision att1

--- mambavision/entries/main_attn1.sh
+++ mambavision/entries/main_attn1.sh
@@ -27,13 +27,14 @@ echo $CUDA_VISIBLE_DEVICES
 DATA_PATH="/lustre/scratch/client/vinai/users/phinh2/workspace/dataset/imagenet"
 MODEL=mamba_vision_T
 BS=128
-EXP=Attn1
+#EXP=Attn1
+EXP=Test
 LR=8e-4
 WD=0.05
 WR_LR=1e-6
 DR=0.38
 MESA=0.25
-RUN_FILE="/lustre/scratch/client/vinai/users/phinh2/workspace/mambavision_1/mambavision/train_attn2.py"
+RUN_FILE="/lustre/scratch/client/vinai/users/phinh2/workspace/mambavision_1/mambavision/train_attn1.py"
 torchrun --master_port 12352 --nproc_per_node=1 $RUN_FILE --mesa ${MESA} --input-size 3 224 224 --crop-pct=0.875 \
 --data_dir=$DATA_PATH --model $MODEL --amp --weight-decay ${WD} --drop-path ${DR} --batch-size $BS --tag $EXP --lr $LR --warmup-lr $WR_LR # \
 # --resume /lustre/scratch/client/vinai/users/phinh2/workspace/mambavision_1/output/train/Original/20240817-001048-mamba_vision_T-224/checkpoint-308.pth.tar
