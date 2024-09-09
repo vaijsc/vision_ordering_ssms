2197440958df628e11ce6c8e92c52e514ee938a1
duc-anh-2002 <nguyenducanh909.bkhn@gmail.com>
2024-09-09 18:14:16 +0000

    update mambavision attn1

--- mambavision/entries/main_attn1.sh
+++ mambavision/entries/main_attn1.sh
@@ -27,8 +27,8 @@ echo $CUDA_VISIBLE_DEVICES
 DATA_PATH="/lustre/scratch/client/vinai/users/phinh2/workspace/dataset/imagenet"
 MODEL=mamba_vision_T
 BS=128
-#EXP=Attn1
-EXP=Test
+EXP=Attn1
+# EXP=Test
 LR=8e-4
 WD=0.05
 WR_LR=1e-6
