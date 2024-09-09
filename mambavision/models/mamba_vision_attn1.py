3d771e7b50e1cb2bbdc96fa13c85733f924f755b
duc-anh-2002 <nguyenducanh909.bkhn@gmail.com>
2024-09-09 10:45:44 +0000

    update mambavision attn1

--- mambavision/models/mamba_vision_attn1.py
+++ mambavision/models/mamba_vision_attn1.py
@@ -666,7 +666,7 @@ class MambaVision(nn.Module):
                                      downsample=(i < 3),
                                      layer_scale=layer_scale,
                                      layer_scale_conv=layer_scale_conv,
-                                     transformer_blocks=0,
+                                     transformer_blocks=[0],
                                      )
             self.levels.append(level)
         self.norm = nn.BatchNorm2d(num_features)
