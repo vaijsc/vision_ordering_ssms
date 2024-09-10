96d708d4c474f30a0764e37133b22eeccb0f6e04
duc-anh-2002 <nguyenducanh909.bkhn@gmail.com>
2024-09-10 09:38:21 +0000

    update mv attn4

---
+++ mambavision/entries/main_attn4.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#SBATCH --job-name=mv_attn4
+#SBATCH --error=/home/anhnd81/anhnd81/workspace/mambavision_1/mambavision/result/mambaV_attn4.txt
+#SBATCH --nodes=1
+#SBATCH --gpus-per-node=1
+#SBATCH --nodelist=sdc2-hpc-dgx-a100-016
+#SBATCH --mem-per-gpu=50G
+#SBATCH --cpus-per-gpu=40
+#SBATCH --partition=research
+#SBATCH --mail-type=all
+#SBATCH --mail-user=v.AnhND81@vinai.io
+
+eval "$(conda shell.bash hook)"
+conda activate /lustre/scratch/client/vinai/users/anhnd81/envs/mambavision
+#conda activate /lustre/scratch/client/vinai/users/phinh2/workspace/envs/mambav
+cd /home/anhnd81/anhnd81/workspace/mambavision_1/mambavision
+#cd /lustre/scratch/client/vinai/users/phinh2/workspace/mambavision_1/mambavision
+echo "Current path is $PATH"
+echo "Running"
+# nvidia-smi
+echo $CUDA_VISIBLE_DEVICES
+
+#Superpod:
+#DATA_PATH="/home/anhnd81/anhnd81/.cache/imagenet/train"
+
+DATA_PATH="/home/anhnd81/anhnd81/.cache/imagenet"
+#DATA_PATH="/lustre/scratch/client/vinai/users/phinh2/workspace/dataset/imagenet"
+MODEL=mamba_vision_T
+BS=128
+# EXP=Attn4
+EXP=Test # after debug change this  -----------------------------------------------------------
+LR=8e-4
+WD=0.05
+WR_LR=1e-6
+DR=0.38
+MESA=0.25
+# RUN_FILE="/lustre/scratch/client/vinai/users/phinh2/workspace/mambavision_1/mambavision/train_attn1.py"
+RUN_FILE="/home/anhnd81/anhnd81/workspace/mambavision_1/mambavision/train_attn4.py"
+torchrun --master_port 12355 --nproc_per_node=1 $RUN_FILE --mesa ${MESA} --input-size 3 224 224 --crop-pct=0.875 \
+--data_dir=$DATA_PATH --model $MODEL --amp --weight-decay ${WD} --drop-path ${DR} --batch-size $BS --tag $EXP --lr $LR --warmup-lr $WR_LR # \
+# --resume /lustre/scratch/client/vinai/users/phinh2/workspace/mambavision_1/output/train/Original/20240817-001048-mamba_vision_T-224/checkpoint-308.pth.tar
