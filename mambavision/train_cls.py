5a6ae8c43dbd05641613bdd24ebb31e094a17a67
duc-anh-2002 <nguyenducanh909.bkhn@gmail.com>
2024-09-10 17:10:14 +0000

    update mv cls

--- mambavision/train_cls.py
+++ mambavision/train_cls.py
@@ -39,7 +39,8 @@ from scheduler.scheduler_factory import create_scheduler
 import shutil
 from utils.datasets import imagenet_lmdb_dataset
 from tensorboard import TensorboardLogger
-from models.mamba_vision_cls import *
+#from models.mamba_vision_cls import *
+from models.mamba_vision import *
 
 try:
     from apex import amp
