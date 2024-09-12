48604ba1b40bd40245930e60c1e935a8682d310d
duc-anh-2002 <nguyenducanh909.bkhn@gmail.com>
2024-09-12 19:04:42 +0000

    update mambavision cls5

--- mambavision/models/mamba_vision_cls4.py
+++ mambavision/models/mamba_vision_cls4.py
@@ -691,7 +691,7 @@ class MambaVisionLayer_reorder(nn.Module):
 
         # After Attention, split cls_token and x
         cls_tokens, x = x[:, :1, :], x[:, 1:, :]
-        # import ipdb; ipdb.set_trace()
+        import ipdb; ipdb.set_trace()
         dot_prod = torch.matmul(x, cls_tokens.transpose(1, 2)).squeeze(2)
         _, rearrange = torch.topk(-1 * dot_prod, k=x.shape[1], dim=1)
         C = x.size(2)  # Number of channels
