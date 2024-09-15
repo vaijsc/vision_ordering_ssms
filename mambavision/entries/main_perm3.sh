964f965efb34f66ce63b91c77c5b65ae378b0428
duc-anh-2002 <nguyenducanh909.bkhn@gmail.com>
2024-09-15 17:07:32 +0000

    update mambavision perm3

--- mambavision/entries/main_perm3.sh
+++ mambavision/entries/main_perm3.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name=mv_perm3
-#SBATCH --error=/home/anhnd81/anhnd81/workspace/mambavision_1/mambavision/result/mambaV_perm3.txt
+#SBATCH --error=/home/anhnd81/anhnd81/workspace/mambavision_1/mambavision/result/mambaV_perm3_1.txt
 #SBATCH --nodes=1
 #SBATCH --gpus-per-node=1
 #SBATCH --nodelist=sdc2-hpc-dgx-a100-018
@@ -25,9 +25,9 @@ echo $CUDA_VISIBLE_DEVICES
 DATA_PATH="/home/anhnd81/anhnd81/.cache/imagenet"
 MODEL=mamba_vision_T
 BS=128
-#EXP=perm3
-EXP=Test # change this after debugging
-LR=8e-4
+EXP=perm3
+#EXP=Test # change this after debugging
+LR=4e-4 # original 8e-4 (loss nan)
 WD=0.05
 WR_LR=1e-6
 DR=0.38
@@ -36,5 +36,5 @@ MESA=0.25
 RUN_FILE="/home/anhnd81/anhnd81/workspace/mambavision_1/mambavision/train_perm3.py"
 # checkpoint="/home/anhnd81/anhnd81/workspace/mambavision_1/output/train/perm1/20240908-234939-mamba_vision_T-224/last.pth.tar"
 torchrun --master-port=12363 --nproc_per_node=1 $RUN_FILE --mesa ${MESA} --input-size 3 224 224 --crop-pct=0.875 \
---data_dir=$DATA_PATH --model $MODEL --amp --weight-decay ${WD} --drop-path ${DR} --batch-size $BS --tag $EXP --lr $LR --warmup-lr $WR_LR \
---resume /home/anhnd81/anhnd81/workspace/mambavision_1/output/train/perm3/20240915-010830-mamba_vision_T-224/model_best.pth.tar
+--data_dir=$DATA_PATH --model $MODEL --amp --weight-decay ${WD} --drop-path ${DR} --batch-size $BS --tag $EXP --lr $LR --warmup-lr $WR_LR #\
+#--resume /home/anhnd81/anhnd81/workspace/mambavision_1/output/train/perm3/20240915-010830-mamba_vision_T-224/model_best.pth.tar
