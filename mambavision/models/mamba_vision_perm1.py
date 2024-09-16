b63877c3854377236f2c169f4168cf5e12cf0d95
duc-anh-2002 <nguyenducanh909.bkhn@gmail.com>
2024-09-16 10:47:28 +0000

    update files

--- mambavision/models/mamba_vision_perm1.py
+++ mambavision/models/mamba_vision_perm1.py
@@ -819,7 +819,7 @@ class MambaVision(nn.Module):
     #     return x
 
     def forward(self, x):
-        breakpoint()
+        # breakpoint()
         x = self.forward_features(x)
         # import ipdb; ipdb.set_trace()
         x = self.head(x)
