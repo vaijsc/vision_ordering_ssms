92d5de3de1850c655b2d7059f836368fdf0f4587
duc-anh-2002 <nguyenducanh909.bkhn@gmail.com>
2024-09-16 03:25:57 +0000

    train perm1 L2 4 GPUs

--- mambavision/entries/main_L2_4GPUs.sh
+++ mambavision/entries/main_L2_4GPUs.sh
@@ -23,7 +23,7 @@ echo "Current path is $PATH"
 echo "Running"
 # nvidia-smi
 echo $CUDA_VISIBLE_DEVICES
-conda activate vssm
+
 #Superpod:
 #DATA_PATH="/home/anhnd81/anhnd81/.cache/imagenet/train"
 #DATA_PATH="/home/anhnd81/anhnd81/.cache/imagenet"
@@ -32,13 +32,12 @@ DATA_PATH="/lustre/scratch/client/vinai/users/ducna22/data/imagenet"
 MODEL=mamba_vision_L2
 BS=128
 EXP=perm1
-#EXP=Test # change after debugging
+#EXP=Test # annotate this after finish debugging
 LR=8e-4
 WD=0.05
 WR_LR=1e-6
 DR=0.38
 MESA=0.25
-#RUN_FILE="/lustre/scratch/client/vinai/users/phinh2/workspace/mambavision_1/mambavision/train.py"
 RUN_FILE="/lustre/scratch/client/vinai/users/ducna22/workspace/mambavision_1/mambavision/train_perm1.py"
 torchrun --master_port 12367 --nproc_per_node=4 $RUN_FILE --mesa ${MESA} --input-size 3 224 224 --crop-pct=0.875 \
 --data_dir=$DATA_PATH --model $MODEL --amp --weight-decay ${WD} --drop-path ${DR} --batch-size $BS --tag $EXP --lr $LR --warmup-lr $WR_LR #\
