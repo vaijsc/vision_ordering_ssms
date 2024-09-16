11d1492a0bad384e4584ddf548118283b9a8e08e
duc-anh-2002 <nguyenducanh909.bkhn@gmail.com>
2024-09-16 02:24:53 +0000

    train large models

--- mambavision/entries/main_L2.sh
+++ mambavision/entries/main_L2.sh
@@ -29,7 +29,8 @@ DATA_PATH="/home/anhnd81/anhnd81/.cache/imagenet"
 #DATA_PATH="/lustre/scratch/client/vinai/users/phinh2/workspace/dataset/imagenet"
 MODEL=mamba_vision_L2
 BS=128
-EXP=perm1
+#EXP=perm1
+EXP=Test # change after debugging
 LR=8e-4
 WD=0.05
 WR_LR=1e-6
@@ -37,6 +38,6 @@ DR=0.38
 MESA=0.25
 #RUN_FILE="/lustre/scratch/client/vinai/users/phinh2/workspace/mambavision_1/mambavision/train.py"
 RUN_FILE="/home/anhnd81/anhnd81/workspace/mambavision_1/mambavision/train_perm1.py"
-torchrun --master_port 12351 --nproc_per_node=1 $RUN_FILE --mesa ${MESA} --input-size 3 224 224 --crop-pct=0.875 \
+torchrun --master_port 12366 --nproc_per_node=1 $RUN_FILE --mesa ${MESA} --input-size 3 224 224 --crop-pct=0.875 \
 --data_dir=$DATA_PATH --model $MODEL --amp --weight-decay ${WD} --drop-path ${DR} --batch-size $BS --tag $EXP --lr $LR --warmup-lr $WR_LR #\
 # --resume /lustre/scratch/client/vinai/users/phinh2/workspace/mambavision_1/output/train/Original/20240817-001048-mamba_vision_T-224/checkpoint-308.pth.tar
