a41ca6bd60d9ee4bc21d2fd9682acf2487dc7471
duc-anh-2002 <nguyenducanh909.bkhn@gmail.com>
2024-09-18 14:47:31 +0700

    update files

--- mambavision/entries/main_L2_4GPUs.sh
+++ mambavision/entries/main_L2_4GPUs.sh
@@ -40,5 +40,5 @@ DR=0.38
 MESA=0.25
 RUN_FILE="/lustre/scratch/client/vinai/users/ducna22/workspace/mambavision_1/mambavision/train_perm1.py"
 torchrun --master_port 12367 --nproc_per_node=4 $RUN_FILE --mesa ${MESA} --input-size 3 224 224 --crop-pct=0.875 \
---data_dir=$DATA_PATH --model $MODEL --amp --weight-decay ${WD} --drop-path ${DR} --batch-size $BS --tag $EXP --lr $LR --warmup-lr $WR_LR #\
-# --resume /lustre/scratch/client/vinai/users/phinh2/workspace/mambavision_1/output/train/Original/20240817-001048-mamba_vision_T-224/checkpoint-308.pth.tar
+--data_dir=$DATA_PATH --model $MODEL --amp --weight-decay ${WD} --drop-path ${DR} --batch-size $BS --tag $EXP --lr $LR --warmup-lr $WR_LR \
+--resume /lustre/scratch/client/vinai/users/ducna22/workspace/mambavision_1/output/train/perm1/20240916-103042-mamba_vision_L2-224/checkpoint-0.pth.tar
