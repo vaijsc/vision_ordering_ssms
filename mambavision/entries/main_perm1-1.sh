ec48175138b5fc3614033141c28e46330b063cf2
duc-anh-2002 <nguyenducanh909.bkhn@gmail.com>
2024-09-18 04:04:55 +0000

    update file perm1-1

--- mambavision/entries/main_perm1-1.sh
+++ mambavision/entries/main_perm1-1.sh
@@ -11,8 +11,8 @@ echo "Using GPUs: $CUDA_VISIBLE_DEVICES"
 DATA_PATH="/root/workspace/dataset/ImageNet1K/data"
 MODEL=mamba_vision_T
 BS=128
-#EXP=perm1
-EXP=Test
+EXP=perm1
+#EXP=Test
 LR=8e-4 # original 8e-4
 WD=0.05
 WR_LR=1e-6
@@ -22,7 +22,7 @@ MESA=0.25
 RUN_FILE="/root/workspace/mambavision_1/mambavision/train_perm1.py"
 # checkpoint="/home/anhnd81/anhnd81/workspace/mambavision_1/output/train/perm1/20240908-234939-mamba_vision_T-224/last.pth.tar"
 torchrun --master-port=12365 --nproc_per_node=4 $RUN_FILE --mesa ${MESA} --input-size 3 224 224 --crop-pct=0.875 \
---data_dir=$DATA_PATH --model $MODEL --amp --weight-decay ${WD}  --batch-size $BS --tag $EXP --lr $LR --warmup-lr $WR_LR \
+--data_dir=$DATA_PATH --model $MODEL --amp --weight-decay ${WD} --batch-size $BS --tag $EXP --lr $LR --warmup-lr $WR_LR \
 > '/root/workspace/mambavision_1/mambavision/result/log_p1-1.txt' 2>&1
 
 # --resume /home/anhnd81/anhnd81/workspace/mambavision_1/output/train/perm1/20240910-162332-mamba_vision_T-224/checkpoint-307.pth.tar
