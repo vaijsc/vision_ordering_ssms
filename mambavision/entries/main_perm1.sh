00ab9148bab3169e1ce6aaf30efd90ed564b3877
duc-anh-2002 <nguyenducanh909.bkhn@gmail.com>
2024-09-20 09:35:35 +0000

    update files

--- mambavision/entries/main_perm1.sh
+++ mambavision/entries/main_perm1.sh
@@ -3,7 +3,7 @@
 #SBATCH --error=/home/anhnd81/anhnd81/workspace/mambavision_1/mambavision/result/mambaV_perm1_2.txt
 #SBATCH --nodes=1
 #SBATCH --gpus-per-node=1
-#SBATCH --nodelist=sdc2-hpc-dgx-a100-020
+#SBATCH --nodelist=sdc2-hpc-dgx-a100-018
 #SBATCH --mem-per-gpu=50G
 #SBATCH --cpus-per-gpu=40
 #SBATCH --partition=research
