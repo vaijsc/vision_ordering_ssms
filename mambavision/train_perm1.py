3517bfec34598812f0fd3271e5879ebc584960f9
duc-anh-2002 <nguyenducanh909.bkhn@gmail.com>
2024-09-21 07:43:35 +0000

    update files

--- mambavision/train_perm1.py
+++ mambavision/train_perm1.py
@@ -39,7 +39,7 @@ from scheduler.scheduler_factory import create_scheduler
 import shutil
 from utils.datasets import imagenet_lmdb_dataset
 from tensorboard import TensorboardLogger
-from models.mamba_vision_perm1 import *
+from models.mamba_vision_perm1_4 import *
 
 try:
     from apex import amp
