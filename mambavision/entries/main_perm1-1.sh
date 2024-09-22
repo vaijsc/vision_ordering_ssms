24eabebc77c62076163191d48c1e2ac5430a1343
duc-anh-2002 <nguyenducanh909.bkhn@gmail.com>
2024-09-22 11:07:00 +0000

    update mv 1-1

--- mambavision/entries/main_perm1-1.sh
+++ mambavision/entries/main_perm1-1.sh
@@ -25,8 +25,8 @@ echo $CUDA_VISIBLE_DEVICES
 DATA_PATH="/home/anhnd81/anhnd81/.cache/imagenet"
 MODEL=mamba_vision_T
 BS=128
-#EXP=perm1-1
-EXP=Test
+EXP=perm1-1
+#EXP=Test
 LR=8e-4 # original 8e-4
 WD=0.05
 WR_LR=1e-6
