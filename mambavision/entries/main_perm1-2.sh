0d1f05c2095a309ff263f59deb224734188bcac2
duc-anh-2002 <nguyenducanh909.bkhn@gmail.com>
2024-09-22 10:59:20 +0000

    update mv 1-1

--- mambavision/entries/main_perm1-2.sh
+++ mambavision/entries/main_perm1-2.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH --job-name=mv_perm1
-#SBATCH --error=/home/anhnd81/anhnd81/workspace/mambavision_1/mambavision/result/mambaV_perm1_3.txt
+#SBATCH --job-name=mvp1-2
+#SBATCH --error=/lustre/scratch/client/vinai/users/phinh2/workspace/mambavision_1/mambavision/result/mambaV_p1-2.txt
 #SBATCH --nodes=1
-#SBATCH --gpus-per-node=1
-#SBATCH --nodelist=sdc2-hpc-dgx-a100-018
+#SBATCH --gpus-per-node=4
+#SBATCH --nodelist=sdc2-hpc-dgx-a100-017
 #SBATCH --mem-per-gpu=50G
 #SBATCH --cpus-per-gpu=40
 #SBATCH --partition=research
@@ -11,8 +11,10 @@
 #SBATCH --mail-user=v.AnhND81@vinai.io
 
 eval "$(conda shell.bash hook)"
-conda activate /lustre/scratch/client/vinai/users/anhnd81/envs/mambavision
-cd /home/anhnd81/anhnd81/workspace/mambavision_1/mambavision
+#conda activate /lustre/scratch/client/vinai/users/anhnd81/envs/mambavision
+conda activate /lustre/scratch/client/vinai/users/phinh2/workspace/envs/mambav
+#cd /home/anhnd81/anhnd81/workspace/mambavision_1/mambavision
+cd /lustre/scratch/client/vinai/users/phinh2/workspace/mambavision_1/mambavision
 echo "Current path is $PATH"
 echo "Running"
 # nvidia-smi
@@ -21,21 +23,19 @@ echo $CUDA_VISIBLE_DEVICES
 #Superpod:
 #DATA_PATH="/home/anhnd81/anhnd81/.cache/imagenet/train"
 
-#DATA_PATH="/home/ubuntu/workspace/dataset/imagenet"
-DATA_PATH="/home/anhnd81/anhnd81/.cache/imagenet"
+#DATA_PATH="/home/anhnd81/anhnd81/.cache/imagenet"
+DATA_PATH="/lustre/scratch/client/vinai/users/phinh2/workspace/dataset/imagenet"
 MODEL=mamba_vision_T
 BS=128
-EXP=perm1
+EXP=perm1-2
 #EXP=Test
-LR=8e-4 # original 8e-4
+LR=8e-4
 WD=0.05
 WR_LR=1e-6
 # DR=0.2
 MESA=0.25
-#RUN_FILE="/home/ubuntu/workspace/mambavision_1/mambavision/train_attn.py"
-RUN_FILE="/home/anhnd81/anhnd81/workspace/mambavision_1/mambavision/train_perm1_3.py"
-# checkpoint="/home/anhnd81/anhnd81/workspace/mambavision_1/output/train/perm1/20240908-234939-mamba_vision_T-224/last.pth.tar"
-torchrun --master-port=12365 --nproc_per_node=1 $RUN_FILE --mesa ${MESA} --input-size 3 224 224 --crop-pct=0.875 \
---data_dir=$DATA_PATH --model $MODEL --amp --weight-decay ${WD} --batch-size $BS --tag $EXP --lr $LR --warmup-lr $WR_LR #\
-#--resume /home/anhnd81/anhnd81/workspace/mambavision_1/output/train/perm1/20240910-162332-mamba_vision_T-224/checkpoint-307.pth.tar
-#--drop-path ${DR} 
+RUN_FILE="/lustre/scratch/client/vinai/users/phinh2/workspace/mambavision_1/mambavision/train_perm1_2.py"
+torchrun --master_port 12381 --nproc_per_node=4 $RUN_FILE --mesa ${MESA} --input-size 3 224 224 --crop-pct=0.875 \
+ --data_dir=$DATA_PATH --model $MODEL --amp --weight-decay ${WD}  --batch-size $BS --tag $EXP --lr $LR --warmup-lr $WR_LR # \
+# --resume /lustre/scratch/client/vinai/users/phinh2/workspace/mambavision_1/output/train/Original/20240817-001048-mamba_vision_T-224/checkpoint-308.pth.tar
+# --drop-path ${DR}s
