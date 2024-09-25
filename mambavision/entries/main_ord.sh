8860b6919407ea2566aca6de2551ffb1e69d93f5
duc-anh-2002 <nguyenducanh909.bkhn@gmail.com>
2024-09-25 18:11:13 +0000

    update files

--- mambavision/entries/main_ord.sh
+++ mambavision/entries/main_ord.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=ord
 #SBATCH --error=/home/anhnd81/anhnd81/workspace/mambavision_1/mambavision/result/mambaV_ord2.txt
 #SBATCH --nodes=1
-#SBATCH --gpus-per-node=1
+#SBATCH --gpus-per-node=4
 #SBATCH --nodelist=sdc2-hpc-dgx-a100-019
 #SBATCH --mem-per-gpu=50G
 #SBATCH --cpus-per-gpu=40
@@ -35,7 +35,7 @@ MESA=0.25
 #RUN_FILE="/home/ubuntu/workspace/mambavision_1/mambavision/train_attn.py"
 RUN_FILE="/home/anhnd81/anhnd81/workspace/mambavision_1/mambavision/train_ord.py"
 # checkpoint="/home/anhnd81/anhnd81/workspace/mambavision_1/output/train/perm1/20240908-234939-mamba_vision_T-224/last.pth.tar"
-torchrun --master-port=12380 --nproc_per_node=1 $RUN_FILE --mesa ${MESA} --input-size 3 224 224 --crop-pct=0.875 \
+torchrun --master-port=12380 --nproc_per_node=4 $RUN_FILE --mesa ${MESA} --input-size 3 224 224 --crop-pct=0.875 \
 --data_dir=$DATA_PATH --model $MODEL --amp --weight-decay ${WD} --batch-size $BS --tag $EXP --lr $LR --warmup-lr $WR_LR #\
 #--resume /home/anhnd81/anhnd81/workspace/mambavision_1/output/train/perm1/20240910-162332-mamba_vision_T-224/checkpoint-307.pth.tar
 #--drop-path ${DR} 
