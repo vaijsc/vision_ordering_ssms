55bb0fe5163f7a00a06c6dd8f7cfac84aa64dfea
duc-anh-2002 <nguyenducanh909.bkhn@gmail.com>
2024-09-25 04:21:16 +0000

    update

--- mambavision/entries/main_ord1.sh
+++ mambavision/entries/main_ord1.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=ord1
 #SBATCH --error=/lustre/scratch/client/vinai/users/phinh2/workspace/mambavision_1/mambavision/result/mambaV_ord1.txt
 #SBATCH --nodes=1
-#SBATCH --gpus-per-node=1
+#SBATCH --gpus-per-node=4
 #SBATCH --nodelist=sdc2-hpc-dgx-a100-020
 #SBATCH --mem-per-gpu=50G
 #SBATCH --cpus-per-gpu=40
@@ -27,15 +27,15 @@ echo $CUDA_VISIBLE_DEVICES
 DATA_PATH="/lustre/scratch/client/vinai/users/phinh2/workspace/dataset/imagenet"
 MODEL=mamba_vision_T
 BS=128
-# EXP=ordering1
-EXP=Test
+EXP=ordering1
+# EXP=Test
 LR=8e-4
 WD=0.05
 WR_LR=1e-6
 DR=0.2
 MESA=0.25
 RUN_FILE="/lustre/scratch/client/vinai/users/phinh2/workspace/mambavision_1/mambavision/train_ord1.py"
-torchrun --master_port 12381 --nproc_per_node=1 $RUN_FILE --mesa ${MESA} --input-size 3 224 224 --crop-pct=0.875 \
+torchrun --master_port 12381 --nproc_per_node=4 $RUN_FILE --mesa ${MESA} --input-size 3 224 224 --crop-pct=0.875 \
  --data_dir=$DATA_PATH --model $MODEL --amp --weight-decay ${WD} --drop-path ${DR} --batch-size $BS --tag $EXP --lr $LR --warmup-lr $WR_LR # \
 # --resume /lustre/scratch/client/vinai/users/phinh2/workspace/mambavision_1/output/train/Original/20240817-001048-mamba_vision_T-224/checkpoint-308.pth.tar
 # 
