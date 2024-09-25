419eab3f367f41dfc16c68298cca6563bfaa0141
duc-anh-2002 <nguyenducanh909.bkhn@gmail.com>
2024-09-25 05:02:53 +0000

    update ord

--- mambavision/entries/main_ord.sh
+++ mambavision/entries/main_ord.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name=ord
-#SBATCH --error=/home/anhnd81/anhnd81/workspace/mambavision_1/mambavision/result/mambaV_ord.txt
+#SBATCH --error=/home/anhnd81/anhnd81/workspace/mambavision_1/mambavision/result/mambaV_ords.txt
 #SBATCH --nodes=1
 #SBATCH --gpus-per-node=4
 #SBATCH --nodelist=sdc2-hpc-dgx-a100-019
