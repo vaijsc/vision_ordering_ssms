9de46ba0c6b5903fc1bdfd9aaa3741a8422c5268
duc-anh-2002 <nguyenducanh909.bkhn@gmail.com>
2024-09-25 18:34:19 +0000

    update files

--- mambavision/entries/main_ord_p1.sh
+++ mambavision/entries/main_ord_p1.sh
@@ -36,6 +36,6 @@ MESA=0.25
 RUN_FILE="/home/anhnd81/anhnd81/workspace/mambavision_1/mambavision/train_ord_p1.py"
 # checkpoint="/home/anhnd81/anhnd81/workspace/mambavision_1/output/train/perm1/20240908-234939-mamba_vision_T-224/last.pth.tar"
 torchrun --master-port=12380 --nproc_per_node=4 $RUN_FILE --mesa ${MESA} --input-size 3 224 224 --crop-pct=0.875 \
---data_dir=$DATA_PATH --model $MODEL --amp --weight-decay ${WD} --batch-size $BS --tag $EXP --lr $LR --warmup-lr $WR_LR #\
-#--resume /home/anhnd81/anhnd81/workspace/mambavision_1/output/train/perm1/20240910-162332-mamba_vision_T-224/checkpoint-307.pth.tar
+--data_dir=$DATA_PATH --model $MODEL --amp --weight-decay ${WD} --batch-size $BS --tag $EXP --lr $LR --warmup-lr $WR_LR \
+--resume /home/anhnd81/anhnd81/workspace/mambavision_1/output/train/ordering/20240925-162255-mamba_vision_T-224/checkpoint-37.pth.tar
 #--drop-path ${DR} 
