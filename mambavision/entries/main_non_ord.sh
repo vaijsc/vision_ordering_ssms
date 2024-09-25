4ca0739be498d56ae9efc3bee6b72dc4d6957ba4
duc-anh-2002 <nguyenducanh909.bkhn@gmail.com>
2024-09-25 03:49:29 +0000

    update files

--- mambavision/entries/main_non_ord.sh
+++ mambavision/entries/main_non_ord.sh
@@ -26,7 +26,7 @@ echo $CUDA_VISIBLE_DEVICES
 #DATA_PATH="/home/anhnd81/anhnd81/.cache/imagenet"
 DATA_PATH="/lustre/scratch/client/vinai/users/phinh2/workspace/dataset/imagenet"
 MODEL=mamba_vision_T
-BS=128
+BS=2
 # EXP=non_ord
 EXP=Test
 LR=8e-4
