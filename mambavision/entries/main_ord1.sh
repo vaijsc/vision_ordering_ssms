35a1d2f2e03701c4a32993c41d821e2dfbb5e2ee
duc-anh-2002 <nguyenducanh909.bkhn@gmail.com>
2024-09-26 04:33:28 +0000

    update

--- mambavision/entries/main_ord1.sh
+++ mambavision/entries/main_ord1.sh
@@ -3,9 +3,9 @@
 #SBATCH --error=/lustre/scratch/client/vinai/users/phinh2/workspace/mambavision_1/mambavision/result/mambaV_ord2_4.txt
 #SBATCH --nodes=1
 #SBATCH --gpus-per-node=4
-#SBATCH --nodelist=sdc2-hpc-dgx-a100-017
+#SBATCH --nodelist=sdc2-hpc-dgx-a100-020
 #SBATCH --mem-per-gpu=50G
-#SBATCH --cpus-per-gpu=40
+#SBATCH --cpus-per-gpu=16
 #SBATCH --partition=research
 #SBATCH --mail-type=all
 #SBATCH --mail-user=v.AnhND81@vinai.io
