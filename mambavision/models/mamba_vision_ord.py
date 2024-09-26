f672f6c1beb2d8a6b346858996ae7015f063e3dd
duc-anh-2002 <nguyenducanh909.bkhn@gmail.com>
2024-09-26 03:31:19 +0000

    update mv perm 2-3

--- mambavision/models/mamba_vision_ord.py
+++ mambavision/models/mamba_vision_ord.py
@@ -698,7 +698,7 @@ class MambaVisionLayer_reorder(nn.Module):
         self.soft_sort = SoftSort(hard=True)
 
     def forward(self, x):
-        B, _, H, W = x.shape
+        B, C, H, W = x.shape
 
         if self.transformer_block:
             pad_r = (self.window_size - W % self.window_size) % self.window_size
@@ -719,8 +719,12 @@ class MambaVisionLayer_reorder(nn.Module):
                 # import ipdb; ipdb.set_trace()
                 learn_key = x.mean(dim=1).view(x.shape[0], 1, x.shape[2]) # [B, 1, C]           
                 dot_prod = torch.matmul(x, learn_key.transpose(1,2)).squeeze(2) # [B, N]
-                perm_matrix = self.soft_sort(-1 * dot_prod) # [B, N, N]
-                x = torch.einsum('blk,bkd->bld', perm_matrix, x)       
+                _, rearrange = torch.topk(-1 * dot_prod, k=x.shape[1], dim=1)  # rearrange: [128, 49]
+                rearrange_expanded = rearrange.unsqueeze(-1).expand(-1, -1, C)  # [128, 49, 448]
+                x_reordered = torch.gather(x, 1, rearrange_expanded.long())  # [128, 49, 448]
+                # perm_matrix = self.soft_sort(-1 * dot_prod) # [B, N, N]
+                # x = torch.einsum('blk,bkd->bld', perm_matrix, x)       
+                x = x_reordered
             x = blk(x) 
         if self.transformer_block:
             x = window_reverse(x, self.window_size, Hp, Wp)
