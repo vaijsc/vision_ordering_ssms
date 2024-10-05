c0b03064736a71840ca7d91b02307c7a7b01dac8
duc-anh-2002 <nguyenducanh909.bkhn@gmail.com>
2024-10-05 15:51:33 +0000

    update sh file

--- mambavision/entries/main_1.sh
+++ mambavision/entries/main_1.sh
@@ -25,8 +25,8 @@ echo $CUDA_VISIBLE_DEVICES
 DATA_PATH="/home/anhnd81/anhnd81/.cache/imagenet"
 MODEL=mamba_vision_T
 BS=128
-# EXP=ord1
-EXP=Test
+EXP=ord1
+# EXP=Test
 LR=8e-4 # original 8e-4
 WD=0.05
 WR_LR=1e-6
@@ -35,7 +35,7 @@ MESA=0.25
 #RUN_FILE="/home/ubuntu/workspace/mambavision_1/mambavision/train_attn.py"
 RUN_FILE="/home/anhnd81/anhnd81/workspace/mambavision_1/mambavision/train_1.py"
 # checkpoint="/home/anhnd81/anhnd81/workspace/mambavision_1/output/train/perm1/20240908-234939-mamba_vision_T-224/last.pth.tar"
-torchrun --master-port=12380 --nproc_per_node=1 $RUN_FILE --mesa ${MESA} --input-size 3 224 224 --crop-pct=0.875 \
+torchrun --master-port=12380 --nproc_per_node=4 $RUN_FILE --mesa ${MESA} --input-size 3 224 224 --crop-pct=0.875 \
  --data_dir=$DATA_PATH --model $MODEL --amp --weight-decay ${WD} --batch-size $BS --tag $EXP --lr $LR --warmup-lr $WR_LR #\
 #--resume /home/anhnd81/anhnd81/workspace/mambavision_1/output/train/perm1/20240910-162332-mamba_vision_T-224/checkpoint-307.pth.tar
 #--drop-path ${DR} 
