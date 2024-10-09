80cbaf18302bce9303514acb0117e637e6bbcd83
duc-anh-2002 <nguyenducanh909.bkhn@gmail.com>
2024-10-09 02:16:56 +0000

    test ord1_1

--- mambavision/entries/main_1.sh
+++ mambavision/entries/main_1.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH --job-name=mv_ord
-#SBATCH --error=/home/anhnd81/anhnd81/workspace/mambavision_1/mambavision/result/mambaV_ord1.txt
+#SBATCH --job-name=mv_ord11
+#SBATCH --error=/home/anhnd81/anhnd81/workspace/mambavision_1/mambavision/result/mambaV_ord1_1.txt
 #SBATCH --nodes=1
 #SBATCH --gpus-per-node=4
-#SBATCH --nodelist=sdc2-hpc-dgx-a100-018
+#SBATCH --nodelist=sdc2-hpc-dgx-a100-019
 #SBATCH --mem-per-gpu=50G
 #SBATCH --cpus-per-gpu=40
 #SBATCH --partition=research
@@ -25,7 +25,8 @@ echo $CUDA_VISIBLE_DEVICES
 DATA_PATH="/home/anhnd81/anhnd81/.cache/imagenet"
 MODEL=mamba_vision_T
 BS=128
-EXP=ord1
+# EXP=ord1
+EXP=ord1_1
 # EXP=Test
 LR=8e-4 # original 8e-4
 WD=0.05
@@ -40,4 +41,5 @@ torchrun --master-port=12380 --nproc_per_node=4 $RUN_FILE --mesa ${MESA} --input
 #--resume /home/anhnd81/anhnd81/workspace/mambavision_1/output/train/perm1/20240910-162332-mamba_vision_T-224/checkpoint-307.pth.tar
 #--drop-path ${DR} 
 
-# ordering inside mambamixer block, use torch.topk with order token is mean()
+#ord ordering inside mambamixer block, use torch.topk with order token is mean()
+#ord1-1 ordering inside mambamixer block, use torch.topk with order token is mean(), for each mambamixer block
--- mambavision/train_1.py
+++ mambavision/train_1.py
@@ -39,7 +39,8 @@ from scheduler.scheduler_factory import create_scheduler
 import shutil
 from utils.datasets import imagenet_lmdb_dataset
 from tensorboard import TensorboardLogger
-from models.mamba_vision_1 import *
+# from models.mamba_vision_1 import * ord1
+from models.mamba_vision_1_1 import *
 
 try:
     from apex import amp
