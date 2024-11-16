d5e192b55bbf452181f5c76566c7d36529f2106b
duc-anh-2002 <nguyenducanh909.bkhn@gmail.com>
2024-11-16 18:03:40 +0000

    update

--- mambavision/entries/run.sh
+++ mambavision/entries/run.sh
@@ -14,7 +14,7 @@ WR_LR=1e-6
 DR=0.2
 MESA=0.25
 RUN_FILE="/home/ubuntu/workspace/mambavision_1/mambavision/train_2.py"
-torchrun --master-port=21383 --nproc_per_node=4 $RUN_FILE --mesa ${MESA} --input-size 3 224 224 --crop-pct=0.875 \
+torchrun --master-port=21384 --nproc_per_node=4 $RUN_FILE --mesa ${MESA} --input-size 3 224 224 --crop-pct=0.875 \
  --data_dir=$DATA_PATH --model $MODEL --amp --weight-decay ${WD} --batch-size $BS --tag $EXP --lr $LR --warmup-lr $WR_LR \
 > >(tee -a /home/ubuntu/workspace/mambavision_1/mambavision/result/mambaV_transpool.txt) 2>&1
 # softsort with order token is linear projection of input -> linear projection dim [49 -> 1] - mamba_vision_test.py
