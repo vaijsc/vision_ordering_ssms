89bba7ce55a65c4a7c030e0b702d910ca2f66908
duc-anh-2002 <nguyenducanh909.bkhn@gmail.com>
2024-11-16 01:48:43 +0000

    update

--- mambavision/entries/main_2.sh
+++ mambavision/entries/main_2.sh
@@ -5,7 +5,7 @@
 #SBATCH --gpus-per-node=4
 #SBATCH --nodelist=sdc2-hpc-dgx-a100-016
 #SBATCH --mem-per-gpu=50G
-#SBATCH --cpus-per-gpu=40
+#SBATCH --cpus-per-gpu=24
 #SBATCH --partition=research
 #SBATCH --mail-type=all
 #SBATCH --mail-user=v.AnhND81@vinai.io
