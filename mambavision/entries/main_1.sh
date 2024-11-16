1538fbd7885a7ef564c64ee1d13b5fc830751101
duc-anh-2002 <nguyenducanh909.bkhn@gmail.com>
2024-11-16 18:45:06 +0000

    update

--- mambavision/entries/main_1.sh
+++ mambavision/entries/main_1.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH --job-name=ord_transmam
+#SBATCH --job-name=ord16_transmam
 #SBATCH --error=/home/anhnd81/anhnd81/workspace/mambavision_1/mambavision/result/mambaV_transord.txt
 #SBATCH --nodes=1
 #SBATCH --gpus-per-node=4
-#SBATCH --nodelist=sdc2-hpc-dgx-a100-018
+#SBATCH --nodelist=sdc2-hpc-dgx-a100-016
 #SBATCH --mem-per-gpu=50G
 #SBATCH --cpus-per-gpu=40
 #SBATCH --partition=research
